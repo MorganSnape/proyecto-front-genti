3e36fda08d7918b54d6bb217748d0da2aa2500a1
MorganSnape <gabriellangeles@outlook.com>
2024-02-27 16:05:55 -0500

    Adjust Movements Container

--- src/components/MovementsContainer.vue
+++ src/components/MovementsContainer.vue
@@ -1,9 +1,9 @@
 <script setup>
-import MovementElement from '@/components/MovementElement.vue';
+import MovementElement from '@/components/MovementElement.vue'
 
 import { movementsMock } from '@/mock/movements'
 
-const movements = movementsMock;
+const movements = movementsMock
 
 const props = defineProps({
   title: {
@@ -11,20 +11,23 @@ const props = defineProps({
     default: 'Últimos Movimientos'
   }
 })
-
-
 </script>
 
 <template>
-  <v-sheet class="mt-4 d-flex py-4 flex-column align-center" width="100%" min-height="100%" color="#E1F9FF">
+  <v-sheet
+    class="mt-4 d-flex py-4 flex-column align-center"
+    width="100%"
+    min-height="100%"
+    color="#E1F9FF"
+  >
     <v-sheet class="d-flex flex-row" color="rgba(0,0,0,0)">
       <h2>{{ props.title }}</h2>
-      <v-icon icon="mdi-chart-box"/>
+      <v-icon icon="mdi-chart-box" />
     </v-sheet>
 
-    <v-sheet v-scroll class="mx-2 overflow-y-auto" width="500px" color="#C6D5E7">
-      <v-sheet v-scroll:#scroll-target="onScroll" class="d-flex flex-column" width="100%" style="padding: 12px; gap: 8px">
-        <MovementElement v-for="movement in movements" :key="movement.id" v-bind="movement"/>
+    <v-sheet class="mx-2 mt-4" width="500px">
+      <v-sheet class="d-flex flex-column" width="100%" color="#C6D5E7" style="padding: 12px; gap: 8px; border-radius: 8px;">
+        <MovementElement v-for="movement in movements" :key="movement.id" v-bind="movement" />
       </v-sheet>
     </v-sheet>
   </v-sheet>
--- src/mock/movements.js
+++ src/mock/movements.js
@@ -57,32 +57,32 @@ export const movementsMock = [
       id: 0
     }
   },
-  {
-    id: 4,
-    type: movementTypes.EGRESO,
-    value: 156,
-    date: Date('2024-02-25'),
-    account: {
-      name: 'Efectivo',
-      id: 0
-    },
-    category: {
-      name: 'Casa',
-      id: 0
-    }
-  },
-  {
-    id: 5,
-    type: movementTypes.EGRESO,
-    value: 16,
-    date: Date('2024-02-25'),
-    account: {
-      name: 'Efectivo',
-      id: 0
-    },
-    category: {
-      name: 'Casa',
-      id: 0
-    }
-  }
+  // {
+  //   id: 4,
+  //   type: movementTypes.EGRESO,
+  //   value: 156,
+  //   date: Date('2024-02-25'),
+  //   account: {
+  //     name: 'Efectivo',
+  //     id: 0
+  //   },
+  //   category: {
+  //     name: 'Casa',
+  //     id: 0
+  //   }
+  // },
+  // {
+  //   id: 5,
+  //   type: movementTypes.EGRESO,
+  //   value: 16,
+  //   date: Date('2024-02-25'),
+  //   account: {
+  //     name: 'Efectivo',
+  //     id: 0
+  //   },
+  //   category: {
+  //     name: 'Casa',
+  //     id: 0
+  //   }
+  // }
 ]
