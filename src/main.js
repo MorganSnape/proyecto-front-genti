0dfbbc32c412dfb052356dceda77743bcd7a8607
MorganSnape <gabriellangeles@outlook.com>
2024-02-28 16:14:57 -0500

    ajuste de color pimario de la librería de diseño

--- src/main.js
+++ src/main.js
@@ -9,10 +9,21 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import colors from 'vuetify/util/colors'
 
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const vuetify = createVuetify({
+  theme: {
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: colors.indigo.darken3,
+        }
+      },
+    },
+  },
   icons: {
     defaultSet: 'mdi',
     aliases,
