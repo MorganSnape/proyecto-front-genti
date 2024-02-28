5d883f3c2de425ad47bd55d29fe285722ebdc531
MorganSnape <gabriellangeles@outlook.com>
2024-02-28 16:28:20 -0500

    maquetar vista de cuentas

---
+++ src/components/AccountsContainer.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+  accounts: {type: Object, required: true}
+})
+const accounts = props.accounts
+
+</script>
+
+<template>
+  <v-sheet color="#C6D5E7" width="100%" class="account-container">
+    <v-chip-group selected-class="text-primary" column>
+      <v-chip
+        closable
+        link
+        :to="'?account=' + account.id"
+        v-for="account in accounts"
+        :key="account.id"
+        prepend-icon="mdi-shape"
+        color="primary"
+        >{{ account.name }}</v-chip
+      >
+    </v-chip-group>
+  </v-sheet>
+</template>
+
+<style scoped>
+.v-sheet.account-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 8px;
+  padding: 12px 16px;
+  border-radius: 12px;
+}
+
+</style>
--- src/views/AccountsView.vue
+++ src/views/AccountsView.vue
@@ -1,11 +1,26 @@
 <script setup>
+import AccountsContainer from '@/components/AccountsContainer.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
+
+import { accountsMock } from '@/mock/accounts';
+
+const accounts = accountsMock
 </script>
 
 <template>
   <AppLayout>
     <v-container>
-      <h1>Billeteras</h1>
+      <h1>Billeteras <v-icon icon="mdi-wallet" /></h1>
+    </v-container>
+
+    <v-container>
+      <v-spacer />
+
+      <v-row justify="center">
+        <v-column>
+          <AccountsContainer :accounts="accounts"/>
+        </v-column>
+      </v-row>
     </v-container>
   </AppLayout>
 </template>
